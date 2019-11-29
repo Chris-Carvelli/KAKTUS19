e5e0f12320b35ce083438aa2de25432234918edf
Mads <xenon373@gmail.com>
2019-11-30 00:24:01 +0100

    Added Menu Triggers for the RayCast Menu picker

---
+++ Assets/Source/MenuTrigger.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.Events;
+
+[Serializable]
+public class MenuTrigger : MonoBehaviour
+{
+    [SerializeField]
+    public UnityEvent actions;
+
+    public void Run()
+    {
+        actions.Invoke();
+    }
+}
+
--- Assets/Source/RaycastMenuPicker.cs
+++ Assets/Source/RaycastMenuPicker.cs
@@ -4,15 +4,59 @@ using UnityEngine;
 
 public class RaycastMenuPicker : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [Tooltip("Main Camera in the scene")]
+    public Camera mainCamera;
+    [Tooltip("How long to focus on a menu element before it's picked")]
+    public float focusTime = 1.5f;
+
+    private RaycastHit raycastHit;
+    private GameObject focusedMenuItem = null;
+    private float elapsedFocusTime = 0.0f;
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (mainCamera != null)
+        {
+            Ray ray = mainCamera.ViewportPointToRay(new Vector3(0.5F, 0.5F, 0));
+            // We hit something
+            if (Physics.Raycast(ray, out raycastHit))
+            {
+                // Is it a menu object?
+                if (raycastHit.collider.gameObject.tag.Equals("menuObject"))
+                {
+                    // do we currently have a some sort of menu item we are looking at?
+                    if (focusedMenuItem != null)
+                    {
+                        // add to elapsed time if we do
+                        if (elapsedFocusTime < focusTime)
+                        {
+                            elapsedFocusTime += Time.deltaTime;
+                        }
+                        else
+                        {
+                            // Call menu function if time has passed
+                            elapsedFocusTime = 0.0f;
+                            focusedMenuItem.GetComponent<MenuTrigger>().Run();
+                        }
+                    }
+                    else
+                    {
+                        // We just found something we wanna focus on
+                        focusedMenuItem = raycastHit.collider.gameObject;
+                    }
+                }
+                else
+                {
+                    // We have not found anything to focus on.
+                    focusedMenuItem = null;
+                    elapsedFocusTime = 0.0f;
+                }
+            }
+        }
+        else
+        {
+            Debug.LogError("No camera in RaycastMenuPicker");
+        }
     }
 }

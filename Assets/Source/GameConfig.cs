7ad569d1de4239e3d91042408fd4e28562696c82
Mads <xenon373@gmail.com>
2019-11-30 14:37:43 +0100

    Shared Game Data Files created

---
+++ Assets/Source/GameConfig.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+[Serializable]
+public class GameConfig : ScriptableObject
+{
+    [SerializeField]
+    public Color[] ObjectColors;
+}

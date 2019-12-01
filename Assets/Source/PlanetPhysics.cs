06068fa3fbcb56353a826acc468288d50a642510
Chris-Carvelli <carve.chris@gmail.com>
2019-12-01 10:55:41 +0100

    revamped physics

--- Assets/Source/PlanetPhysics.cs
+++ Assets/Source/PlanetPhysics.cs
@@ -5,7 +5,9 @@ using UnityEngine;
 public class PlanetPhysics : MonoBehaviour
 {
 	public float gravity = 9.81f;
+    public float stickForce = 10;
 	public Rigidbody target;
+    public Rigidbody secondaryTarget = null;
 
     public float travelScale;
     public float landedScale;
@@ -14,12 +16,12 @@ public class PlanetPhysics : MonoBehaviour
     private float _forceScale;
     private bool _collided = false;
 
-    [Header("Joint config")]
-    public float breakForce;
-    public float breakTorque;
+    // [Header("Joint config")]
+    // public float breakForce;
+    // public float breakTorque;
 
 	[Header("Debug")]
-    float force;
+    public float force;
     public float velocityMagnitude;
     public float forceMultiplier;
     public float distance;
@@ -30,7 +32,7 @@ public class PlanetPhysics : MonoBehaviour
 
 	private Rigidbody body;
     private Rigidbody _originalTarget;
-    FixedJoint _stickJoint;
+    // FixedJoint _stickJoint;
     // Start is called before the first frame update
     void Start()
     {
@@ -45,14 +47,23 @@ public class PlanetPhysics : MonoBehaviour
     {
         distance = Vector3.Distance(target.transform.position, transform.position);
     	dir = (target.transform.position - transform.position).normalized;
-    	magnitude = dir.magnitude;
         force = (gravity * (body.mass * target.mass)) / (useDistance ? distance : 1);
         forceMultiplier = _forceScale;
         force *= forceMultiplier;
 
+        magnitude = force;
         velocityMagnitude = body.velocity.magnitude;
-        if (!_collided)
-            body.AddForce(dir * force, ForceMode.Acceleration);
+        // if (!_collided)
+        // body.AddForce(dir * force, ForceMode.Acceleration);
+
+        body.AddForce(dir * force, ForceMode.Acceleration);
+        if (secondaryTarget != null) {
+            body.AddForce((secondaryTarget.transform.position - transform.position).normalized * stickForce, ForceMode.Acceleration);
+            // GetComponent<PickUp>().SetColor(Color.red);
+        }
+        else {
+            // GetComponent<PickUp>().SetColor(Color.blue);
+        }
         /*else if (velocityMagnitude * 2 > breakForce) {
             Destroy(_stickJoint);
             _collided = false;
@@ -60,16 +71,17 @@ public class PlanetPhysics : MonoBehaviour
     }
 
     public void OnCollisionEnter(Collision c) {
-        /*print(name);
-        _forceScale = landedScale;*/
+        //print(name);
+        _forceScale = landedScale;
 
-        if (!_collided && attractToCollided) {
-            body.velocity = Vector3.zero;
-            body.angularVelocity = Vector3.zero;
-            _stickJoint = gameObject.AddComponent<FixedJoint>();
-            _stickJoint.breakForce = breakForce;
-            //_stickJoint.breakTorque = breakTorque;
-            _stickJoint.connectedBody = target;
+        if (!_collided && attractToCollided && c.transform != target.transform) {
+            secondaryTarget = c.transform.GetComponent<Rigidbody>();
+            // body.velocity = Vector3.zero;
+            // body.angularVelocity = Vector3.zero;
+            // _stickJoint = gameObject.AddComponent<FixedJoint>();
+            // _stickJoint.breakForce = breakForce;
+            // //_stickJoint.breakTorque = breakTorque;
+            // _stickJoint.connectedBody = target;
         }
 
         //body.isKinematic = true;
@@ -77,12 +89,12 @@ public class PlanetPhysics : MonoBehaviour
         _collided = true;
     }
 
-    public void OnJointBreak(float f) {
-        print($"breaking force: {f}");
-        body.AddForce(-(target.transform.position - transform.position).normalized,ForceMode.Impulse);
-        _collided = false;
-        _forceScale = travelScale;
-    }
+    // public void OnJointBreak(float f) {
+    //     print($"breaking force: {f}");
+    //     body.AddForce(-(target.transform.position - transform.position).normalized,ForceMode.Impulse);
+    //     _collided = false;
+    //     _forceScale = travelScale;
+    // }
     /*public void OnCollisionExit(Collision c) {
         _forceScale = travelScale;
         target = _originalTarget;

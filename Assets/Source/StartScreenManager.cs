ef2b77ef10d3fe41066a63d5b68dad4016df4a35
Mads <xenon373@gmail.com>
2019-12-01 13:30:17 +0100

    Planet Start Screen done ?

--- Assets/Source/StartScreenManager.cs
+++ Assets/Source/StartScreenManager.cs
@@ -5,6 +5,12 @@ using UnityEngine.SceneManagement;
 
 public class StartScreenManager : MonoBehaviour
 {
+    public Camera _main;
+    public Transform playText;
+    public PlanetController _planetController;
+
+    public bool _Play = true;
+
     public void StartGame(int sceneIndex)
     {
         SceneManager.LoadScene(sceneIndex);
@@ -13,5 +19,24 @@ public class StartScreenManager : MonoBehaviour
     public void Exit()
     {
         Application.Quit();
+    }
+
+    public void Update()
+    {
+        _Play = Vector3.Dot(_main.transform.position, playText.forward) > 0.3f;
+        if (_Play == true)
+        {
+            if (_planetController.accelMagnitude > 5)
+            {
+                StartGame(1);
+            }
+        }
+        else
+        {
+            if (_planetController.accelMagnitude > 5)
+            {
+                Exit();
+            }
+        }
     }
 }

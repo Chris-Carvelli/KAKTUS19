84f243d86fd36b5a7c1d1ef66774c8aa34d2829a
Mikkel Juul <mjoj@itu.dk>
2019-12-01 11:50:15 +0100

    Implemented FMOD + Banks
    Here we go boys

---
+++ Assets/Source/MusicManager.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using FMOD;
+using FMODUnity;
+using UnityEngine.SceneManagement;
+
+public class MusicManager : MonoBehaviour
+{
+    public static MusicManager Instance;
+    public FMOD.Studio.EventInstance SplashMusic;
+    public FMOD.Studio.EventInstance GameMusic;
+
+
+
+    void Awake()
+    {
+        if (Instance != null)
+        {
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            Instance = this;
+            DontDestroyOnLoad(this.gameObject);
+        }
+
+        SplashMusic = FMODUnity.RuntimeManager.CreateInstance("event:/Music/SplashMusic");
+        GameMusic = FMODUnity.RuntimeManager.CreateInstance("event:/Music/GameMusic");
+    }
+
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        SplashMusic.start();
+    }
+
+    // Update is called once per frame
+    void Update()
+    { }
+
+
+    void ChangeToGameMusic()
+    {
+        SplashMusic.stop(FMOD.Studio.STOP_MODE.ALLOWFADEOUT);
+        GameMusic.start();
+    }
+}

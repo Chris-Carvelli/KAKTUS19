134c4a424ff212930b089c7727e1ab22de493dd3
Mads <xenon373@gmail.com>
2019-12-01 12:29:19 +0100

    Auto stash before merge of "master" and "origin/master"

---
+++ Assets/RotateAround.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateAround : MonoBehaviour
+{
+    public float rotationSpeed;
+    public Vector3 rotationPoint;
+    public Vector3 rotationAxes;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.RotateAround(rotationPoint, rotationAxes, rotationSpeed);
+    }
+}
